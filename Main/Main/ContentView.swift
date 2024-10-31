22a51855553687127ce29ddbed66881e821e8de2
thomasg13 <thomasguo13@gmail.com>
2024-10-31 18:54:14 -0400

    Add Project

---
+++ Main/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Main
+//
+//  Created by Thomas Guo on 10/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Main/Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by Thomas Guo on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
