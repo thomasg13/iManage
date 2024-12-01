56093ad349859f16066f4bab0eee71e40f2fcdd9
ivanli05 <ivan_li_hz@outlook.com>
2024-11-30 20:46:12 -0500

    loading page

--- Main/Main/Views/LoadingScreenView.swift
+++ Main/Main/Views/LoadingScreenView.swift
@@ -8,18 +8,33 @@
 import SwiftUI
 
 struct LoadingScreenView: View {
-    var body: some View {
-        ZStack{
-            Color.white
-                .edgesIgnoringSafeArea(.all)
-            
-            Text("imanage")
-                .font(.title)
-                
-        }
-    }
+	@State private var textOpacity: Double = 0.0
+	private let text = "iManage"
+	var body: some View {
+		
+		ZStack{
+			Color.gray.opacity(0.2)
+				.edgesIgnoringSafeArea(.all)
+			
+			HStack(spacing: 0) {
+				ForEach(0..<text.count, id: \.self) { index in
+					Text(String(text[text.index(text.startIndex, offsetBy: index)]))
+						.font(.title)
+						.bold()
+						.foregroundStyle(.secondary)
+						.background(.ultraThinMaterial)
+						.background(Color(red: 0.1+Double(index) * 0.2, green: 0.2+Double(index) * 0.2, blue: 0.5+Double(index) * 0.2).gradient)
+						.opacity(self.textOpacity==0 ? 1 : 0)
+						.animation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true).delay(Double(index) * 0.2))
+						.onAppear {
+							self.textOpacity = 1.0
+						}
+				}
+			}
+		}
+	}
 }
 
 #Preview {
-    LoadingScreenView()
+	LoadingScreenView()
 }
