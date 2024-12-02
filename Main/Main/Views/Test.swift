5b3ab6c6081f01bb61467c9c7bc57b7abb46fad5
thomasg13 <thomasguo13@gmail.com>
2024-12-01 21:40:13 -0500

    Add Ivan change

---
+++ Main/Main/Views/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Main
+//
+//  Created by Thomas Guo on 12/1/24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
